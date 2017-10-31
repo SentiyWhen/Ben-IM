a9a5bf4a7dfe0269d1467103b79f38acdfedb0ee
张文文 <18622076193@163.com>
2017-10-31 16:09:02 +0800

    注册 加密 存储 读取等等

--- server/user.js
+++ server/user.js
@@ -1,9 +1,40 @@
 const express = require('express')
+const utils = require('utility')
+
 const Router = express.Router()
+const model = require('./model')
+const User = model.getModel('user')
+
+Router.get('/list',function(req, res){
+	// User.remove({},function(e,d){})
+	User.find({},function(err,doc){
+		return res.json(doc)
+	})
+})
+Router.post('/register', function(req, res){
+	const {user, pwd, type} = req.body
+	User.findOne({user},function(err,doc){
+		if (doc) {
+			return res.json({code:1,msg:'用户名重复'})
+		}
+
+		User.create({user,type,pwd:md5Pwd(pwd)},function(e,d){
+			if (e) {
+				return res.json({code:1,msg:'后端出错了'})
+			}
+			return res.json({code:0})
+		})
+	})
+})
 
 Router.get('/info',function(req, res){
     // 用户有没有cookie
     return res.json({code:1})
 })
+
+function md5Pwd(pwd){
+	const salt = 'ben_is_good_3957x8yza6!@#IUHJh~~'
+	return utils.md5(utils.md5(pwd+salt))
+}
 
 module.exports = Router
--- src/container/register/register.js
+++ src/container/register/register.js
@@ -1,14 +1,23 @@
 import React from 'react'
 import Logo from '../../component/logo/logo'
 import { Button, WhiteSpace, WingBlank, List, InputItem, Radio } from 'antd-mobile'
-
+import { connect } from 'react-redux'
+import { register } from '../../redux/user.redux'
+import { Redirect } from 'react-router-dom'
 const RadioItem = Radio.RadioItem;
+
+@connect(
+    state=>state.user,
+    {register}
+)
+
+
 
 class Register extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            userName: '',
+            user: '',
             pwd: '',
             repeatPwd: '',
             type:'genius'
@@ -16,6 +25,7 @@ class Register extends React.Component {
 
     }
     registerClick(){
+        this.props.register(this.state);
         console.log(`${JSON.stringify(this.state)}`);
     }
     handleChang(key,val){
@@ -27,12 +37,13 @@ class Register extends React.Component {
     render() {
         return (
             <div>
+                {this.props.redirectTo? <Redirect to={this.props.redirectTo} />:null}
                 <Logo/>
                 <WingBlank>
                     <List>
-                        <InputItem onChange={v=>this.handleChang('userName',v)}>用户名</InputItem><WhiteSpace />
-                        <InputItem onChange={v=>this.handleChang('pwd',v)}>密码</InputItem><WhiteSpace />
-                        <InputItem onChange={v=>this.handleChang('repeatPwd',v)}>确认密码</InputItem><WhiteSpace />
+                        <InputItem onChange={v=>this.handleChang('user',v)}>用户名</InputItem><WhiteSpace />
+                        <InputItem type='password' onChange={v=>this.handleChang('pwd',v)}>密码</InputItem><WhiteSpace />
+                        <InputItem type='password' onChange={v=>this.handleChang('repeatPwd',v)}>确认密码</InputItem><WhiteSpace />
                         <RadioItem checked={this.state.type=='genius'} onChange={()=>this.handleChang('type','genius')}>牛人</RadioItem><WhiteSpace />
                         <RadioItem checked={this.state.type=='boss'} onChange={()=>this.handleChang('type','boss')}>Boss</RadioItem><WhiteSpace />
                     </List><WhiteSpace />
