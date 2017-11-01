6e392fd401b3da39af8ae40ca270f8ed90c55d21
张文文 <18622076193@163.com>
2017-11-01 16:10:57 +0800

    完善页

--- src/container/register/register.js
+++ src/container/register/register.js
@@ -1,57 +1,77 @@
 import React from 'react'
 import Logo from '../../component/logo/logo'
-import { Button, WhiteSpace, WingBlank, List, InputItem, Radio } from 'antd-mobile'
-import { connect } from 'react-redux'
-import { register } from '../../redux/user.redux'
-import { Redirect } from 'react-router-dom'
-const RadioItem = Radio.RadioItem;
+import {List, InputItem,Radio, WingBlank, WhiteSpace, Button} from 'antd-mobile'
+import {connect} from 'react-redux'
+import {Redirect} from 'react-router-dom'
+import {regisger} from '../../redux/user.redux'
 
 @connect(
-    state=>state.user,
-    {register}
+	state=>state.user,
+	{regisger}
 )
+class Register extends React.Component{
+	constructor(props) {
+		super(props)
+		this.state = {
+			user:'',
+			pwd:'',
+			repeatpwd:'',
+			type:'genius' // 或者boss
+		}
+
+		this.handleRegister = this.handleRegister.bind(this)
+	}
+	handleChange(key,val){
+		this.setState({
+			[key]:val
+		})
+	}
+	handleRegister(){
+		this.props.regisger(this.state)
+	}
+	render(){
+		const RadioItem = Radio.RadioItem
+		return (
+			<div>
+				{this.props.redirectTo? <Redirect to={this.props.redirectTo} />:null}
+				<Logo></Logo>
+				<List>
+					{this.props.msg?<p className='error-msg'>{this.props.msg}</p>:null}
+					<InputItem
+						onChange={v=>this.handleChange('user',v)}
+					>用户名</InputItem>
+					<WhiteSpace />
+					<InputItem
+						type='password'
+						onChange={v=>this.handleChange('pwd',v)}
+					>密码</InputItem>
+					<WhiteSpace />
+					<InputItem
+						type='password'
+						onChange={v=>this.handleChange('repeatpwd',v)}
+					>确认密码</InputItem>
+					<WhiteSpace />
+					<RadioItem
+						checked={this.state.type=='genius'}
+						onChange={()=>this.handleChange('type','genius')}
+					>
+						牛人
+					</RadioItem>
+					<RadioItem
+						checked={this.state.type=='boss'}
+						onChange={()=>this.handleChange('type','boss')}
+					>
+						BOSS
+					</RadioItem>
+					<WhiteSpace />
+					<Button type='primary' onClick={this.handleRegister}>注册 </Button>
+				</List>
 
 
+			</div>
 
-class Register extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            user: '',
-            pwd: '',
-            repeatPwd: '',
-            type:'genius'
-        }
-
-    }
-    registerClick(){
-        this.props.register(this.state);
-        console.log(`${JSON.stringify(this.state)}`);
-    }
-    handleChang(key,val){
-        this.setState({
-            [key]:val
-        })
-    }
-
-    render() {
-        return (
-            <div>
-                {this.props.redirectTo? <Redirect to={this.props.redirectTo} />:null}
-                <Logo/>
-                <WingBlank>
-                    <List>
-                        <InputItem onChange={v=>this.handleChang('user',v)}>用户名</InputItem><WhiteSpace />
-                        <InputItem type='password' onChange={v=>this.handleChang('pwd',v)}>密码</InputItem><WhiteSpace />
-                        <InputItem type='password' onChange={v=>this.handleChang('repeatPwd',v)}>确认密码</InputItem><WhiteSpace />
-                        <RadioItem checked={this.state.type=='genius'} onChange={()=>this.handleChang('type','genius')}>牛人</RadioItem><WhiteSpace />
-                        <RadioItem checked={this.state.type=='boss'} onChange={()=>this.handleChang('type','boss')}>Boss</RadioItem><WhiteSpace />
-                    </List><WhiteSpace />
-                    <Button type="primary" onClick={this.registerClick.bind(this)}>注册</Button><WhiteSpace />
-                </WingBlank>
-            </div>
-        )
-    }
+		)
+	}
 }
 
 export default Register
--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDom from 'react-dom'
 import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
-import { BrowserRouter,	Route} from 'react-router-dom'
+import { BrowserRouter, Route, Redirect,Switch} from 'react-router-dom'
 
 import Login from './container/login/login'
 import Register from './container/register/register'
 import AuthRoute from './component/authroute/authroute'
+import BossInfo from './container/bossinfo/bossinfo'
+import GeniusInfo from './container/geniusinfo/geniusinfo'
 import reducers from './reducer'
 import './config'
 
@@ -15,19 +17,18 @@ const store = createStore(reducers, compose(
 	applyMiddleware(thunk),
 	window.devToolsExtension?window.devToolsExtension():f=>f
 ))
-function Boss(){
-	return <h2>BOSSyemi</h2>
-}
 
 ReactDom.render(
 	(<Provider store={store}>
 		<BrowserRouter>
          <div>
 		 	 <AuthRoute/>
-             <Route path='/login' component={Login}></Route>
-             <Route path='/register' component={Register}></Route>
-			 <Route path='/boss' component={Boss}></Route>
-
+			 <Switch>
+	             <Route path='/login' component={Login}></Route>
+	             <Route path='/register' component={Register}></Route>
+				 <Route path='/bossinfo' component={BossInfo}></Route>
+				 <Route path='/geniusinfo' component={GeniusInfo}></Route>
+			 </Switch>
          </div>
 		</BrowserRouter>
 	</Provider>),
