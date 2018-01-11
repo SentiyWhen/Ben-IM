d5976188fcd1d0cfca2937187bc5ecd812de3f71
张文文 <18622076193@163.com>
2018-01-11 16:53:13 +0800

    init

--- benim/server/server.js
+++ benim/server/server.js
@@ -1,52 +1,13 @@
 const express = require('express')
-const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
 
-//link mongo
-const DB_URL = 'mongodb://127.0.0.1:27017/ben'
-mongoose.connect(DB_URL)
-mongoose.connection.on('connected',function(){
-    console.log('mongo connect success!')
-})
-//类似于mysql的表 mongo里有文档、字段的概念
-const User = mongoose.model('user',new mongoose.Schema({
-    user:{type:String,require:true},
-    age:{type:Number,require:true}
-}))
-//新增数据
-// User.create({
-//     user:'xiaoying',
-//     age:26
-// },function(err,doc){
-//     if (!err) {
-//         console.log(doc)
-//     } else {
-//         console.log(err)
-//     }
-// })
-//移除
-// User.remove({age:1},function(err,doc){
-//     console.log(doc)
-// })
-//修改
-// User.update({'user':'dawen'},{'$set':{age:99}},function(err,doc){
-//     console.log(doc)
-// })
-//new app
+const userRouter = require('./user')
+
 const app = express()
-
-app.get('/',function(req,res){
-    res.send('<h1>Hello Express<h1/>')
-})
-
-app.get('/data',function(req,res){
-    // User.find({},function(err,doc){
-    //     res.json(doc)
-    // })
-    User.findOne({user:'dawen'},function(err,doc){
-        res.json(doc)
-    })
-})
-
+app.use(cookieParser())
+app.use(bodyParser.json())
+app.use('/user',userRouter)
 app.listen(9093,function(){
-    console.log('Node app start at port 9093!')
+	console.log('Node app start at port 9093')
 })
