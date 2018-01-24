5d4973210cd8c2dd289fb57d1e2ab43276026d87
张文文 <18622076193@163.com>
2018-01-24 16:56:03 +0800

    header  footer

--- benim/src/index.js
+++ benim/src/index.js
@@ -10,6 +10,7 @@ import Register from './container/register/register'
 import BossInfo from './container/bossinfo/bossinfo'
 import GeniusInfo from './container/geniusinfo/geniusinfo'
 import AuthRoute from './component/authroute/authroute'
+import Dashboard from './component/dashboard/dashboard'
 import reducers from './reducer'
 import './config'
 import './index.css'
@@ -18,18 +19,19 @@ const store = createStore(reducers, compose(
 	applyMiddleware(thunk),
 	window.devToolsExtension?window.devToolsExtension():f=>f
 ))
-function Boss(){
-	return <h2>BOSS页面</h2>
-}
+
 ReactDom.render(
 	(<Provider store={store}>
 		<BrowserRouter>
 			<div>
 				<AuthRoute></AuthRoute>
-				<Route path='/bossinfo' component={BossInfo}></Route>
-				<Route path='/geniusinfo' component={GeniusInfo}></Route>				
-				<Route path='/login' component={Login}></Route>
-				<Route path='/register' component={Register}></Route>
+				<Switch>
+					<Route path='/bossinfo' component={BossInfo}></Route>
+					<Route path='/geniusinfo' component={GeniusInfo}></Route>				
+					<Route path='/login' component={Login}></Route>
+					<Route path='/register' component={Register}></Route>
+					<Route component={Dashboard}></Route>
+				</Switch>
 			</div>
 		</BrowserRouter>
 	</Provider>),
