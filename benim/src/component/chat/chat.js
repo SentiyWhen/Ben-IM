50553cfaa073a5965132d19383731e99239e1638
张文文 <18622076193@163.com>
2018-01-30 10:45:30 +0800

    已读消息

--- benim/src/component/chat/chat.js
+++ benim/src/component/chat/chat.js
@@ -3,14 +3,14 @@ import {List,InputItem,NavBar,Icon, Grid} from 'antd-mobile'
 import io from 'socket.io-client'
 import {connect} from 'react-redux'
 import {getChatId} from '../../util'
-import {getMsgList,sendMsg,recvMsg} from '../../redux/chat.redux'
+import {getMsgList,sendMsg,recvMsg,readMsg} from '../../redux/chat.redux'
 
 
 const socket = io('ws://localhost:9093')
 
 @connect(
 	state=>state,
-	{ getMsgList, sendMsg, recvMsg}
+	{ getMsgList, sendMsg, recvMsg,readMsg}
 )
 
 class Chat extends React.Component{
@@ -24,6 +24,10 @@ class Chat extends React.Component{
 			this.props.recvMsg()	
         }
     }
+    componentWillUnmount(){
+		const to = this.props.match.params.user
+		this.props.readMsg(to)
+	}
     fixCarousel(){
 		setTimeout(function(){
 			window.dispatchEvent(new Event('resize'))
